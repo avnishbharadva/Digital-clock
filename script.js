b33a9c1e81cc30c60b64c85ca84752ec6bb76a08
Avnish Bharadva <avnishbharadva@gmail.com>
2021-12-31 18:49:45 +0530

    Digital Clock

---
+++ script.js
@@ -0,0 +1,23 @@
+var date = document.querySelector('#date');
+var day = document.querySelector('.day');
+var month = document.querySelector('.month');
+var year = document.querySelector('.year');
+var time = document.querySelector('#time');
+
+setInterval(ddate,1000);
+
+function ddate(){
+var d = new Date();
+
+day.innerHTML = d.getDate();
+month.innerHTML = d.getMonth();
+year.innerHTML = d.getFullYear();
+}
+
+setInterval(dtime,1000);
+
+function dtime(){
+    t = new Date();
+    // time.innerHTML = t.getHours() + " " + t.getMinutes() + " " + t.getSeconds();
+    time.innerHTML = t.toLocaleTimeString();
+}
